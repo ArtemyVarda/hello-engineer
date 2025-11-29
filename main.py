5c0f4ae15eb1557f452e71d8e2bd28ff3869f7b1
Артем <artemii.vardanian@yandex.ru>
2025-11-30 00:43:01 +0400

    git remote add origin https://github.com/ArtemyVarda/hello-engineer.git

---
+++ main.py
@@ -0,0 +1,7 @@
+author_name = "Artemy Vardanian" 
+
+artist_group = 'Scorpions'
+song1 = 'Wind of Change'
+song2 = 'Maybe I Maybe You'
+print(author_name)
+print(f'Моя любимая группа {artist_group}. Сейчас я слушаю {song1} и {song2}')
